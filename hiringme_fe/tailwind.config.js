0ba10284796010079108754a77468bcb19db31c4
RiskiFadillah <riski.fadil95@gmail.com>
2023-01-31 15:22:15 +0700

    modified:   hiringme_fe/tailwind.config.js

--- hiringme_fe/tailwind.config.js
+++ hiringme_fe/tailwind.config.js
@@ -4,15 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary : "#FBB017",
+        primary: "#FBB017",
         secondary: "#5E50A1",
-        header : "#1F2A36",
-        text : "#46505C",
-        pale : "#9EA0A5",
+        header: "#1F2A36",
+        text: "#46505C",
+        pale: "#9EA0A5",
         placeholder: "#E2E5ED",
-        
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
