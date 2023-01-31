220335bf1498bd7c3d4009967710c07aa34fbad5
Rizky208 <rizky160@gmail.com>
2023-01-31 15:23:20 +0700

    modified:   hiringme_be/helper(db)/connection.js

--- hiringme_be/helper(db)/connection.js
+++ hiringme_be/helper(db)/connection.js
@@ -1,1 +1,19 @@
 const { Client } = require("pg");
+
+const db = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "aaa",
+  password: "123",
+  port: 5432,
+});
+
+db.connect((err) => {
+  if (!err) {
+    console.log("Database CONNECTED");
+  } else {
+    console.log("Database error connection", err);
+  }
+});
+
+module.exports = db;
