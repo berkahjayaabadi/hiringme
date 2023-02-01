bca9f1d56ec61bb89d801e3bae7015183de4bb75
jiamudza <jiamudza@gmail.com>
2023-02-01 15:33:10 +0700

    add login page

--- hiringme_fe/src/App.js
+++ hiringme_fe/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, LandingPages } from "./pages";
+import { Home, LandingPages, Login } from "./pages";
 import './assets/css/index.css'
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<LandingPages />} />
+        <Route path="/auth/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
